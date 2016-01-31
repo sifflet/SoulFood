e710882b0b399a3cab57b53797b8a4822ad8208c
Ajayveer Singh Aujla <ajay.aujla@hotmail.com>
2016-01-31 16:59:03 -0500

    Create title screen allowing selection to play between angel or death

---
+++ GameName/Assets/TitleScreenButtonManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreenButtonManager : MonoBehaviour {
+
+    public enum CharacterToPlayAs { Death = 1, Angel = 2};
+
+    public static CharacterToPlayAs playingAs = CharacterToPlayAs.Death;
+
+    void Update ()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            playingAs = CharacterToPlayAs.Death;
+            Application.LoadLevel(1);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            playingAs = CharacterToPlayAs.Angel;
+            Application.LoadLevel(1);
+        }
+    }
+}
