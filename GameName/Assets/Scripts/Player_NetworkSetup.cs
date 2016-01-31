3e4b6a8fdbc61f9ceb52d0121b97ce6653354599
Kevin <kevin.tcoutu@gmail.com>
2016-01-30 20:26:01 -0500

    Network prototype

---
+++ GameName/Assets/Scripts/Player_NetworkSetup.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Player_NetworkSetup : NetworkBehaviour
+{
+    [SerializeField]
+    Camera FPSCharacterCam;
+
+    [SerializeField]
+    AudioListener audioListener;
+
+	void Start ()
+    {
+        if (isLocalPlayer)
+        {
+            GameObject.Find("Main Camera").SetActive(false);
+            GetComponent<CharacterController>().enabled = true;
+            GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = true;
+            FPSCharacterCam.enabled = true;
+            audioListener.enabled = true;
+        }
+	}
+}
---
+++ GameName/Assets/Scripts/Player_SyncPosition.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Player_SyncPosition : NetworkBehaviour
+{
+    [SyncVar]
+    private Vector3 syncPos;
+
+    [SerializeField]
+    Transform transform;
+    [SerializeField]
+    float lerpRate = 15;
+
+	void FixedUpdate ()
+    {
+        TransmitPosition();
+        LerpPosition();
+	}
+
+    void LerpPosition()
+    {
+        if (!isLocalPlayer)
+        {
+            transform.position = Vector3.Lerp(transform.position, syncPos, Time.deltaTime * lerpRate);
+        }
+    }
+
+    [Command]
+    void CmdProvivePositionToServer(Vector3 pos)
+    {
+        syncPos = pos;
+    }
+
+    [ClientCallback]
+    void TransmitPosition()
+    {
+        if (isLocalPlayer)
+        {
+            CmdProvivePositionToServer(transform.position);
+        }
+    }
+}
