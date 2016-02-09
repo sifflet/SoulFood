e24009726aa0e989915e44412ae260c3cb9eb46c
DavidPaparo <david_paparo@hotmail.com>
2016-02-09 12:19:55 -0500

    Tree/Buttons Scripts and Prefabs
    -Created Tree Script
    -Created Button Script
    -Created Single Button Prefab
    -Created Single Tree Prefab
    -Created Tree (1 Button) Prefab
    -Created Tree (2 Button) Prefab
    -Created Tree (3 Button) Prefab

---
+++ GameName/Assets/Scripts/Game Objects/Tree.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tree : MonoBehaviour {
+	int triggeredCount;
+	public GameObject souls; //Attach soul prefab here
+
+
+	void Update () {
+		triggeredCount = 0;
+		foreach (Transform button in transform) {
+			if(button.gameObject.GetComponent<Button>().getButtonStatus())
+				triggeredCount++;
+		}
+		if (triggeredCount++ == transform.childCount) {
+			dropItems();
+		}
+	}
+
+	void dropItems(){
+		//Dump whatever items you need
+	}
+}
