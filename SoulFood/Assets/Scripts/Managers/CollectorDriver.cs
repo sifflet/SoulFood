54e254a00037af0fa80cb26cfa30d9ca16ab9630
DavidPaparo <david_paparo@hotmail.com>
2016-04-07 20:00:46 -0400

    Conflicts ~

--- SoulFood/Assets/Scripts/Managers/CollectorDriver.cs
+++ SoulFood/Assets/Scripts/Managers/CollectorDriver.cs
@@ -4,15 +4,37 @@ using System.Collections.Generic;
 
 public class CollectorDriver : NPCDriver
 {
-	private const float MAX_SPEED = 15f;
+    private const float MAX_SPEED = 15f;
     private int soulsStored = 0;
-    public float eatingDelay = 0.5f;
-	public int SoulsStored { get { return this.soulsStored; } }
+    private bool isImmortal = false;
+    private float immortalityTimer = 0f;
 
-    public override void OnStartLocalPlayer()
+    public int SoulsStored { get { return this.soulsStored; } }
+
+    public bool IsImmortal
     {
-        cameraDriver.enabled = true;
+        get { return this.isImmortal; }
+
+        set
+        {
+            if (value)
+            {
+                this.isImmortal = true;
+                this.immortalityTimer = GameManager.IMMORTALITY_TIME;
+
+                if (this.controlledByAI)
+                {
+                    this.stateMachine.enabled = false;
+                }
+                else
+                {
+                    this.keyboardInputs.enabled = false;
+                }
+            }
+        }
     }
+
+    public GameObject soulPrefab;
 
     public override void Setup(GameObject instance, GameObject cameraInstance, Transform spawnPoint)
     {
@@ -36,14 +58,63 @@ public class CollectorDriver : NPCDriver
         this.stateMachine.Setup(this);
     }
 
-    public void AddSoul()
+    public override void Update()
     {
-        this.soulsStored++;
+        base.Update();
+
+        if (isImmortal)
+        {
+            immortalityTimer -= Time.deltaTime;
+
+            if (immortalityTimer < 0)
+            {
+                if (this.controlledByAI)
+                {
+                    this.stateMachine.enabled = true;
+                }
+                else
+                {
+                    this.keyboardInputs.enabled = true;
+                }
+
+                isImmortal = false;
+            }
+        }
+    }
+
+    /*
+     *  Acquire soul prefab from GameManager since cannot set the soul prefab through the inspector on this script
+     */
+    public void SetSoulPrefab(GameObject soulPrefab)
+    {
+        this.soulPrefab = soulPrefab;
     }
 
-    public void DropSoul()
+    public void AddSoul()
     {
-        this.soulsStored--;
+        ++this.soulsStored;
+    }
+
+    public void DropSoul(int soulsDropped)
+    {
+        if (this.soulsStored > 0)
+        {
+            this.soulsStored -= soulsDropped;
+            InstantiateSouls(soulsDropped);
+        }
+    }
+
+    private void InstantiateSouls(int numberOfSouls)
+    {
+        Vector3 explosionVector = new Vector3(transform.position.x, transform.position.y + 2.5f, transform.position.z + 2f);
+        float explosionMagnitude = 350f;
+        float explosionRadius = 5f;
+
+        for (int i = 0; i < numberOfSouls; ++i)
+        {
+            GameObject soul = (GameObject)Instantiate(soulPrefab, new Vector3(transform.position.x, transform.position.y + 5, transform.position.z), Quaternion.identity);
+            soul.GetComponent<Rigidbody>().AddExplosionForce(explosionMagnitude, explosionVector, explosionRadius);
+        }
     }
 
     protected override void FindVisibleNPCs()
