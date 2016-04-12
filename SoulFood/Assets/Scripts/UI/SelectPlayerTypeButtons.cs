28977798d32ba38ed7c626b2056c1f37ede18dcd
Stephanie Li <stephanie.zhongsui.li@gmail.com>
2016-04-12 05:17:57 -0400

    Made UI buttons for lobby page to select guard or collector

---
+++ SoulFood/Assets/Scripts/UI/SelectPlayerTypeButtons.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectPlayerTypeButtons : MonoBehaviour {
+	
+	private string buttonName;
+
+	// Use this for initialization
+	void Start () {
+		buttonName = this.name;
+	}
+
+	public void LoadOnClick()
+	{
+		LobbyPlayer lobbyPlayer = FindObjectOfType(typeof(LobbyPlayer)) as LobbyPlayer;
+		
+		switch (buttonName) 
+		{
+		case "Button_Collector":
+			lobbyPlayer.PlayerType = "Collector";
+			break;
+		case "Button_Guard":
+			lobbyPlayer.PlayerType = "Guard";
+			break;
+		}
+	}
+}
